377fa5b230487840b292159155266babbeae5e17
5arg <37589694+5arg@users.noreply.github.com>
2020-11-06 11:02:07 +0100

    File/folder structure for landing page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import axios from "axios";
-import { useQuery } from "react-query";
+import Landing from "./pages/Landing";
 
 function App() {
-  const { isLoading, error, data } = useQuery(
-    "fetchPlayers",
-    () => axios("http://localhost:3000/players"),
-    {
-      onSuccess: (data: any) => console.log(data),
-    }
-  );
-
   return (
     <div className="App">
-      <img src={`data:image/png;base64,${data?.data[0].profilePicture}`} />
+      <Landing />
     </div>
   );
 }
---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Jumbotron from "../features/jumbotron/Jumbotron";
+
+export default function Landing() {
+  return (
+    <div>
+      <Jumbotron />
+    </div>
+  );
+}
