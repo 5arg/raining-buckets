5059fb905bcd81fef5d6c64f809dc5fadeceeda9
5arg <37589694+5arg@users.noreply.github.com>
2020-11-07 15:41:27 +0100

    Added search player button and some basic stylings for jumbotron elements.

--- frontend/src/features/jumbotron/Jumbotron.tsx
+++ frontend/src/features/jumbotron/Jumbotron.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
-import { Background } from "./styles/jumbotron";
+import {
+  Background,
+  Title,
+  SubTitle,
+  AnimationWrapper,
+  SearchPlayerButton,
+} from "./styles/jumbotron";
 import ball from "../../assets/ball.png";
 
 export default function Jumbotron() {
   const props = useSpring({
     to: async (next: any, cancel: any) => {
       await next({ bottom: 0 });
-      await next({ bottom: 50 });
+      await next({ bottom: 60 });
       await next({ bottom: 0 });
     },
     from: {
@@ -16,9 +22,25 @@ export default function Jumbotron() {
       height: 50,
       color: "red",
       position: "absolute",
+      margin: "auto",
+      left: 0,
+      right: 0,
     },
     config: { duration: 300 },
+    delay: 800,
   });
 
-  return <Background />;
+  return (
+    <Background>
+      <Title>
+        <span style={{ fontSize: 86 }}>R</span>aining{" "}
+        <span style={{ fontSize: 86 }}>B</span>uckets
+      </Title>
+      <SubTitle>Active NBA players stats. Quick. Beautiful.</SubTitle>
+      <AnimationWrapper>
+        <animated.img src={ball} style={props} />
+      </AnimationWrapper>
+      <SearchPlayerButton>Search Player</SearchPlayerButton>
+    </Background>
+  );
 }
--- frontend/src/features/jumbotron/styles/jumbotron.tsx
+++ frontend/src/features/jumbotron/styles/jumbotron.tsx
@@ -3,10 +3,56 @@ import bgCourt from "../../../assets/bg-court.jpg";
 
 export const Background = styled.div`
   height: 100vh;
-  background: linear-gradient(0deg, rgb(255, 255, 255, 0.7), rgba(0, 0, 0, 0.5)),
+  background: linear-gradient(
+      0deg,
+      rgb(255, 255, 255, 0.7),
+      rgba(40, 40, 40, 0.3)
+    ),
     url(${bgCourt});
   background-size: cover;
   background-position: center center;
   background-repeat: no-repeat;
   background-attachment: fixed;
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  text-transform: uppercase;
+  padding-top: 90px;
+  font-size: 70px;
+  font-family: CarroisGothicSC-Regular;
+  color: #333333;
+  letter-spacing: 3.5px;
+`;
+
+export const SubTitle = styled.h3`
+  text-transform: uppercase;
+  margin-top: 10px;
+  font-family: CarroisGothicSC-Regular;
+  color: #606060;
+  letter-spacing: 2px;
+`;
+
+export const AnimationWrapper = styled.div`
+  margin-top: 400px;
+  position: relative;
+`;
+
+export const SearchPlayerButton = styled.button`
+  background-color: transparent;
+  color: #333333;
+  border: 1px solid #333333;
+  border-radius: 5px;
+  padding: 15px 30px;
+  text-transform: uppercase;
+  font-size: 24px;
+  letter-spacing: 2px;
+  transition: background-color 0.8s, color 0.8s;
+  &:hover {
+    cursor: pointer;
+    background-color: #333333;
+    color: white;
+    opacity: 0.8;
+  }
 `;
